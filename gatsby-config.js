0d156d1e859565cf054e6d2be92e57c6d8dd60a9
John <johnsmilga@gmail.com>
2020-06-05 09:27:58 -0700

    changes in gatsby cofing

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,17 +25,18 @@ module.exports = {
         path: `${__dirname}/src/assets/`,
       },
     },
-    // {
-    //   resolve: `gatsby-source-strapi`,
-    //   options: {
-    //     apiURL: `http://localhost:1337`,
-    //     queryLimit: 1000, // Default to 100
-    //     // contentTypes: [`jobs`, `projects`, `blogs`, ],
-    //     //singleTypes:[`about` ]
-    //     contentTypes: [`jobs`, `projects`, `blogs`],
-    //     singleTypes: [`about`],
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        //   contentTypes : `jobs`, `projects`, `blogs`,
+        //   singleType : `about`
+        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
+        contentTypes: [],
+        singleTypes: [],
+      },
+    },
     // {
     //   resolve: `gatsby-plugin-prefetch-google-fonts`,
     //   options: {
